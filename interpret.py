b029c4bc373f60d7bbf26d832de326213dd584d5
AnimusVonith <jakub.adamciak@gmail.com>
2020-04-04 16:12:05 +0200

    Update interpret.py

--- interpret.py
+++ interpret.py
@@ -13,26 +13,31 @@ if 1>len(sys.argv)>2:
 
 def err_exit(err_code):
     switcher = {
-        1: "Error occured"
+        1: "Error occured",
+        2: "help err",
+        3: "else err"
     }
     print(switcher.get(err_code, "Error in error printing"))
     exit(err_code)
 
-for arg in sys.argv:
+for arg in sys.argv[1:]:
     if re.match('--input=.+', arg):
         input=arg.split('=')[1]
     elif re.match('--source=.+', arg):
         source=arg.split('=')[1]
     elif re.match('--help', arg):
         if len(sys.argv)>1:
-            err_exit(err)
+            err_exit(2)
         else:
             print("WOW! This is useless\n")
             exit(no_err)
     else:
-        err_exit(err)
+        err_exit(3)
 
 if input == "":
     input = sys.stdin.read()
 elif source == "":
     source = sys.stdin.read()
+
+print("done")
+exit(0)
