23e15658b06f008ac4f6c8336088761e1b99250b
AnimusVonith <jakub.adamciak@gmail.com>
2020-04-08 18:41:55 +0200

    Update interpret.py

--- interpret.py
+++ interpret.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-import sys
-import re
+from sys import stdin, argv
+from re import match
 
 input_path = ""
 source_path = ""
@@ -9,7 +9,7 @@ context = []
 
 
 def err_exit(err_code):
-    switcher = {
+    switch_err = {
         1: "par len err",
         2: "help err",
         3: "else err",
@@ -17,14 +17,26 @@ def err_exit(err_code):
         5: "match err",
         6: "context err"
     }
-    print(switcher.get(err_code, "Error in error printing"))
+    print(switch_err.get(err_code, "Error in error printing"))
     exit(err_code)
 
+def path_handler(checked_path):
+    if checked_path == "":
+        return stdin
+    else:
+        try:
+            return open(checked_path, 'r')
+        except:
+            err_exit(4)
 
 def context_entry(checked_word):
-    if checked_word == "program" and context != []:
+    if checked_word != "program" or checked_word != "instruction" or match('arg[1-4]', checked_word) == 0:
         err_exit(6)
-    if context[-1] == checked_word or re.match(checked_word + '[1-9][0-9]*', context[-1]):
+    elif checked_word == "program" and context != []:
+        err_exit(6)
+    elif checked_word == "instruction" and context[-1] != "program":
+        err_exit(6)
+    elif match('arg[1-4]', checked_word) and context[-1] != "instruction":
         err_exit(6)
     context.push(checked_word)
 
@@ -36,30 +48,29 @@ def context_exit(checked_word):
         err_exit(6)
 
 
-if len(sys.argv) < 2 or len(sys.argv) > 3:
-    err_exit(err)
+if len(argv) < 2 or len(argv) > 3:
+    err_exit(1)
 
-for arg in sys.argv[1:]:
-    if re.match('--input\=.+', arg):
-        input_path = arg.split('=')
-    elif re.match('--source\=.+', arg):
+for arg in argv[1:]:
+    if match('--input\=.+', arg):
+        input_path = arg.split('=')[1]
+    elif match('--source\=.+', arg):
         source_path = arg.split('=')[1]
-    elif re.match('--help', arg):
-        if len(sys.argv) > 2:
+    elif match('--help', arg):
+        if len(argv) > 2:
             err_exit(2)
         else:
             print("WOW! This is useless\n")
-            exit(no_err)
+            exit(0)
     else:
         err_exit(3)
 
-if input_path == "":
-    input_path = "sys.stdin"
-if source_path == "":
-    source_path = "sys.stdin"
 
-source_file = open(source_path, 'r')
-if re.match('<\?xml version="1.0" encoding="UTF-8"\?>', source_file.readline()) == 0:
+input_file=path_handler(input_path)
+source_file=path_handler(source_path)
+
+
+if match('<\?xml version="1.0" encoding="UTF-8"\?>', source_file.readline()) == 0:
     err_exit(5)
 
 for line in source_file.readlines():
