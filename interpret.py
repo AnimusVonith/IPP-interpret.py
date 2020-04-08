5ce1c541a7cf3cb8bac03970dcd46234766c0ea4
AnimusVonith <jakub.adamciak@gmail.com>
2020-04-08 22:24:43 +0200

    Update interpret.py

--- interpret.py
+++ interpret.py
@@ -15,7 +15,9 @@ def err_exit(err_code):
         3: "else err",
         4: "file err",
         5: "match err",
-        6: "context err"
+        6: "context err",
+        7: "semantic err",
+        8: "command err"
     }
     print(switch_err.get(err_code, "Error in error printing"))
     exit(err_code)
@@ -29,20 +31,26 @@ def path_handler(checked_path):
         except:
             err_exit(4)
 
+
 def context_entry(checked_word):
-    if checked_word != "program" or checked_word != "instruction" or match('arg[1-4]', checked_word) == 0:
+    if checked_word != "program" and checked_word != "instruction" and match('arg[1-4]', checked_word) == 0:
         err_exit(6)
     elif checked_word == "program" and context != []:
         err_exit(6)
-    elif checked_word == "instruction" and context[-1] != "program":
+    elif len(context) == 0 and checked_word != "program":
+        err_exit(6)
+    elif checked_word == "instruction" and context == [-1]:
         err_exit(6)
     elif match('arg[1-4]', checked_word) and context[-1] != "instruction":
         err_exit(6)
-    context.push(checked_word)
+    context.append(checked_word)
+
 
 
 def context_exit(checked_word):
-    if context[-1] == checked_word:
+    if len(context) == 0:
+        err_exit(6)
+    elif context[-1] == checked_word:
         context.pop()
     else:
         err_exit(6)
@@ -73,16 +81,46 @@ source_file=path_handler(source_path)
 if match('<\?xml version="1.0" encoding="UTF-8"\?>', source_file.readline()) == 0:
     err_exit(5)
 
+instruction_list = []
+instruction_with_args=[]
 
 for line in source_file.readlines():
-    if search('<program language=".+" name=".+">',line):
-        print("program")
-    elif search('<instruction order="[1-9][0-9]*" opcode=".+">',line):
-        print("instruction")
-    elif search('<arg[1-4] type=.+>',line):
-        print("arg")
-    else:
-        print(line)
+    if search('<program language=".+" name=".+">$',line):
+        if match(compile('ippcode20' ,IGNORECASE),line.split(' ')[1].split('=')[1]):
+            err_exit(7)
+        context_entry("program")
 
-print("done")
+    elif search('<instruction order="[1-9][0-9]*" opcode=".+">$' ,line):
+        context_entry("instruction")
+        instruction_with_args=[]
+        instruction = [line.strip().split(' ')[1].split('=')[1].strip('"'),
+                       line.strip().split(' ')[2].split('=')[1].split('>')[0].strip('"')]
+        instruction_with_args.append(instruction)
+
+    elif search('<arg[1-4] type=.+>.*</arg[1-4]>$' ,line):
+        if line.strip()[4] != line.strip()[-2]:
+            err_exit(7)
+        context_entry("arg" +str(line.strip()[4]))
+        argument = [line.strip()[4],
+                    line.strip().split(' ')[1].split('>')[0].split('=')[1].strip('"'),
+                    line.strip().split(' ')[1].split('>')[1].split('<')[0]]
+
+        instruction_with_args.append(argument)
+        context_exit("arg" +str(line.strip()[-2]))
+
+    elif search('</instruction>$' ,line):
+        context_exit("instruction")
+        instruction_list.append(instruction_with_args)
+    elif search('</program>$' ,line):
+        context_exit("program")
+    else:
+        err_exit(8)
+
+def sorting_func(e):
+    return int(e[0][0])
+
+instruction_list.sort(key=sorting_func)
+for i in instruction_list:
+    print(i)
+
 exit(0)
